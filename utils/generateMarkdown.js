5e4199ec7d682121a8480aad52789fd62690e49b
Matt Panyor <matt@panyor.com>
2024-02-16 19:22:15 +0000

    markdown transformer

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+          ## Description
+          ${data.description}
+          ## Table of Contents
+          - [Install instructions](#install-instructions)
+          - [Usage instructions](#usage-instructions)
+          - [License](#license)
+          - [Contribution Rules](#contribution-rules)
+          - [Testing instructions](#testing-instructions)
+          - [Questions](#questions)
+          ## Install instructions
+          ${data.install_desc}
+          ## Usage instructions
+          ${data.usage_desc}
+          ## License
+          ${data.license}
+          ## Contribution Rules
+          ${data.contributing_rules}
+          ## Testing instructions
+          ${data.test_inst}
+          ## Questions
+          My contact information if you have any questions are the following:
+          *Email*: ${data.email}
+          *GitHub*: [User Profile](https://github.com/${data.username})
 `;
 }
 
